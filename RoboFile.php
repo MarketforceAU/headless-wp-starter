726b87e0e97508fae25a3fa0d205a29ff4a4523c
Ben Derham <bderham@marketforce.com.au>
2019-01-07 14:45:52 +0800

    remove wp-migrate-pro

--- RoboFile.php
+++ RoboFile.php
@@ -159,41 +159,6 @@ class RoboFile extends \Robo\Tasks {
     }
 
     /**
-     * Import WordPress data.
-     *
-     * @param  array $opts options
-     * @return void
-     */
-    public function wordpressImport( $opts = [
-        'migratedb-license' => null,
-        'migratedb-from' => null,
-    ] ) {
-        if ( isset( $opts['migratedb-license'] ) ) {
-            $this->wp( 'migratedb setting update license ' . $opts['migratedb-license'] );
-        } else {
-            $this->say( 'WP Migrate DB Pro: no license available. Please set migratedb-license in the robo.yml file.' );
-            return;
-        }
-
-        if ( isset( $opts['migratedb-from'] ) ) {
-            $command = 'WPMDB_EXCLUDE_RESIZED_MEDIA=1 wp migratedb pull ';
-            $command .= $opts['migratedb-from'];
-            $command .= ' --backup=prefix ';
-            $command .= ' --media=compare ';
-            $this->io()->success( 'About to run data migration from ' . $opts['migratedb-from'] );
-            $this->taskExec( $command )->run();
-            // Set siteurl and home
-            $this->wp( 'option update siteurl http://localhost:8080' );
-            $this->wp( 'option update home http://localhost:8080' );
-        } else {
-            $this->say(
-                'WP Migrate DB Pro: No source installation specified. Please set migratedb-from in the robo.yml file.'
-            );
-            return;
-        }
-    }
-
-    /**
      * Run WordPress task.
      *
      * @param  arr $arg Arguments
